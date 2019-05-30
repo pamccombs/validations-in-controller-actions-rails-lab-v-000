adaec3fa3b0214f22e1c13ddb6ef272f135b8a83
Patrick McCombs <pa.mccombs1@gmail.com>
2019-05-30 05:31:21 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
     
-    if @post.update
+    if @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
